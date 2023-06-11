1bb0b086184ea46c1957757c5d5f11f3da58c0a1
ITDhruv <120175094+ITDhruv@users.noreply.github.com>
2023-06-11 20:16:41 +0530

    Day 3 completedW

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-
   final int days = 30;
   final String name = "Dhruwuu";
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
